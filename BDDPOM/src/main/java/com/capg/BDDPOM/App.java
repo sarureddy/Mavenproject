92e6c3627e9d5b83910d8caf072d0aacc05bb117
sarureddy <gogireddy.saraswathi@gmail.com>
2020-08-05 11:49:14 +0530

    child

--- BDDPOM/src/main/java/com/capg/BDDPOM/App.java
+++ BDDPOM/src/main/java/com/capg/BDDPOM/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println( "developer 1" );
+        
+        System.out.println( "chld branch" );
     }
 }
